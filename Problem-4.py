8ac4130ce91f43a016f7613178060666a1547869
2jiyong <jdragon2000@naver.com>
2024-11-25 16:40:50 +0900

    Add function to make first paint

---
+++ Problem-4.py
@@ -0,0 +1,22 @@
+def makePaint():
+    paint=[
+        [' ']*80 for _ in range(30)
+    ]
+
+    for r in range(30):
+        paint[r][0]='|'
+        paint[r][79]='|'
+
+    for c in range(80):
+        paint[0][c]='-'
+        paint[29][c]='-'
+        if c==0 or c==79:
+            paint[0][c]='+'
+            paint[29][c]='+'
+
+    return paint
+
+# for elems in paint:
+#     for elem in elems:
+#         print(elem,end='')
+#     print()
